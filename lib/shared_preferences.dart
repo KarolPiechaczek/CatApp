1402d4cf3e4266b27a84132a40dc08becf410a1a
KarolPiechaczek <piechaczekkarol1@gmail.com>
2024-08-08 11:22:14 +0200

    CAT-8: SharedPref replaced with SharedPrefAsync and iOS issues fixed

--- lib/shared_preferences.dart
+++ lib/shared_preferences.dart
@@ -2,20 +2,16 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class Helper {
   String valueSharedPreferences = 'isPressed';
+  SharedPreferencesAsync asyncSharedPreferences = SharedPreferencesAsync();
 
-  Future<bool> saveIfPressedState(value) async {
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    return await sharedPreferences.setBool(valueSharedPreferences, value);
+  Future<void> saveIfPressedState(value) async {
+    return await asyncSharedPreferences.setBool(valueSharedPreferences, value);
   }
 
   Future<bool> getIfPressedState() async {
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    bool? isButtonPressed = sharedPreferences.getBool(valueSharedPreferences);
-    if (isButtonPressed != null){
-      isButtonPressed = sharedPreferences.getBool(valueSharedPreferences) as bool;
-    } else{
-      isButtonPressed = false;
-    }
-    return isButtonPressed;
+    bool? isButtonPressed;
+    isButtonPressed = await 
+        asyncSharedPreferences.getBool(valueSharedPreferences); 
+    return isButtonPressed ?? false;
   }
 }
